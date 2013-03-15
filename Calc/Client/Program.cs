92f307ddb617a2b5f7fc4a4a58b6c0b2c2df4dd4
anjelica <wysowl88@comcast.net>
2013-03-14 17:30:48 -0700

    got clients working
    to run:
    - set calc as startup program
    - run without debugging
    - set client as startup
    - run without debugging twice

--- Calc/Client/Program.cs
+++ Calc/Client/Program.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.ServiceModel;
+using Client.ServiceReference1;
 
 namespace Client
 {
@@ -11,6 +10,40 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            //Step 1: Create an instance of the WCF proxy.
+            ServiceReference1.CalcServiceClient client = new ServiceReference1.CalcServiceClient();
+
+            //String line = Console.ReadLine();
+
+
+            // Step 2: Call the service operations.
+            // Call the Add service operation.
+            double value1 = Convert.ToInt32(Console.ReadLine());
+            double value2 = Convert.ToInt32(Console.ReadLine());
+            double result = client.Add(value1, value2);
+            Console.WriteLine("Add({0},{1}) = {2}", value1, value2, result);
+
+            // Call the Subtract service operation.
+            value1 = 145.00D;
+            value2 = 76.54D;
+            result = client.Subtract(value1, value2);
+            Console.WriteLine("Subtract({0},{1}) = {2}", value1, value2, result);
+
+            // Call the Multiply service operation.
+            value1 = 9.00D;
+            value2 = 81.25D;
+            result = client.Multiply(value1, value2);
+            Console.WriteLine("Multiply({0},{1}) = {2}", value1, value2, result);
+
+            // Call the Divide service operation.
+            value1 = 22.00D;
+            value2 = 7.00D;
+            result = client.Divide(value1, value2);
+            Console.WriteLine("Divide({0},{1}) = {2}", value1, value2, result);
+
+            //Step 3: Closing the client gracefully closes the connection and cleans up resources.
+            client.Close();
+            Console.ReadLine();
         }
     }
 }
